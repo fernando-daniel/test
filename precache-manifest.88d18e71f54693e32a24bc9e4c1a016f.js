52f24d59e980cbffa18eca4247afd3737a517217
fernandodcastanon <fernandodaniel777castanon@gmail.com>
2019-10-08 19:55:32 -0700

    Updates

---
+++ precache-manifest.88d18e71f54693e32a24bc9e4c1a016f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c18a68f0adcf5b215d29131c1a26f18",
+    "url": "/,github.io/work/index.html"
+  },
+  {
+    "revision": "6178ad49c6a8c547bd8c",
+    "url": "/,github.io/work/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2c96237edad97c300536",
+    "url": "/,github.io/work/static/js/2.f5db97f5.chunk.js"
+  },
+  {
+    "revision": "6178ad49c6a8c547bd8c",
+    "url": "/,github.io/work/static/js/main.4b7c4c00.chunk.js"
+  },
+  {
+    "revision": "805a58b9f411e61b11de",
+    "url": "/,github.io/work/static/js/runtime-main.8910c4c5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/,github.io/work/static/media/logo.25bf045c.svg"
+  }
+]);
